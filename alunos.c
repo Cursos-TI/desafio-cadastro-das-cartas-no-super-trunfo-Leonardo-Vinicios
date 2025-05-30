e9526c4a4bd86af11c0ae5fa649e7df6badd5a7e
Leonardo-Vinicios <leonardo_vinni@outlook.com>
2025-05-30 17:48:03 +0000

    Att1

--- alunos.c
+++ alunos.c
@@ -3,8 +3,24 @@
 int main () {
 
   int nota1 = 0, nota2 = 0, nota3 = 0;
-  float resultado = 0;
+  float media = 0;
+
+  printf("---PROGRAMA DE CALCULO DA MÉDIA ---\n");
   
+  printf("Digite a sua 1º nota: \n");
+  scanf("%d", &nota1);
+
+  printf("Digite a sua 2º nota: \n");
+  scanf("%d", &nota2);
+
+  printf("Digite a sua 3º nota: \n");
+  scanf("%d", &nota3);
+
+  media = (float) (nota1 + nota2 + nota3) / 3;
+
+  printf("A média é: %f", media);
+
+  return 0;
   
 
 }
