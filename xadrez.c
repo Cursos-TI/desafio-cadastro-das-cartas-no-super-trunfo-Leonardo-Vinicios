d36ea36050da7996e8309279569035ab37a7864f
Leonardo-Vinicios <leonardo_vinni@outlook.com>
2025-06-09 20:12:49 -0300

    Xadrez Nivel Mestre

--- xadrez.c
+++ xadrez.c
@@ -1,35 +1,69 @@
 #include <stdio.h>
 
+void torre(int movimento){
+    if (movimento >= 1)
+    {
+        torre(movimento - 1);
+        printf("%d° movimento -- Torre: direita --> \n", movimento);
+    }
+}
+
+void bispo(int movimento){
+
+    if (movimento >= 1){
+        bispo(movimento - 1);
+        printf("%d° movimento -- Bispo: cima, ", movimento);
+        if (movimento >= 1){
+            printf("direita -->\n");
+        }
+        
+    }
+}
+
+void rainha(int movimento){
+   
+    if (movimento >= 1){ 
+        rainha(movimento - 1);
+        printf("%d° movimento -- Rainha: esquerda <--\n", movimento);
+    }
+    
+}
+
+void cavalo(int i){
+    for (i = 1; i <= 2; i++)
+        printf("%d° movimento -- Cavalo: Cima \n", i);
+        for (int j = 3; j < 4 ; j++){
+            printf("%d° movimento -- Cavalo: Direita --> \n", j);            
+        }
+}
+
 int main () {
-
-    int itorre = 1, ibispo = 1, irainha = 1;
-
+    
+    int itorre = 5, ibispo = 5, irainha = 8, icavalo = 1;
+    
     // Torre
     printf("-- Bem-Vindo ao Xadrez abstrato --\n");
-    printf("Torre (While)\n");
-    printf("Mover 5 casas para direita\n");
-
-    while (itorre <= 5)
-    {
-        printf("%d° movimento -- Torre: direita --> \n", itorre);
-        itorre++;
-    }
+    
+    printf("Torre\n");
+    printf("Mover 5 casas para direita.\n");
+    torre(itorre);
    
     //Bispo
-    printf("\nBispo (Do-while)\n");
-    printf("Mover 5 casas na diagonal\n");
-    do {
-        printf("%d° movimento -- Bispo: cima, direita -->\n", ibispo);
-        ibispo++;
+    printf("\nBispo\n");
+    printf("Mover 5 casas na diagonal.\n");
+    bispo(ibispo);
 
-    } while (ibispo <= 5);
     
     //Rainha
-    printf("\nRainha (For)\n");
-    printf("Mover 8 casa para esquerda\n");
-    for (irainha = 1; irainha <=8; irainha++){
-        printf("%d° movimento -- Rainha: esquerda <--\n", irainha);
-    }
+    printf("\nRainha\n");
+    printf("Mover 8 casas para esquerda.\n");
+    rainha(irainha);
 
+    //Cavalo 
+    printf("\nCavalo\n");
+    printf("Mover 2 casas para cima e 1 para direita. \n");
+    cavalo(icavalo);
+    
+    
     return 0;
 }
